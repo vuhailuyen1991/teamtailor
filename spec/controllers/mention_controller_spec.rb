23e51acb5455e952835cd2b253ac808a2adc6b8b
Luyen Vu <vuhailuyen1991@gmail.com>
2015-03-30 21:16:09 +0700

    Implement Mention Controller

--- spec/controllers/mention_controller_spec.rb
+++ spec/controllers/mention_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe MentionController, type: :controller do
+  let(:huddle) { create :huddle, published: true }
+  describe "create#mention" do
+    it "should return 200 http code" do
+      receiver = create :user, username: 'username1'
+      sender = create :user, username: 'username2'
+      candidate = create :candidate
+      message = 'He is fantastics'
+      user_setting = create :user_setting, user: receiver
 
+      post :create, sender: sender.username, receiver: receiver.username, message: message, candidate_id: candidate.id
+      response.status.should eq 200
+    end
+
+    it "should return 401 http code" do
+      receiver = create :user, username: 'username1'
+      candidate = create :candidate
+      message = 'He is fantastics'
+
+      post :create, receiver: receiver.username, message: message, candidate_id: candidate.id
+      response.status.should eq 400
+    end
+  end
 end
--- spec/factories/user_settings.rb
+++ spec/factories/user_settings.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user_setting do
-    
+    association :user
   end
 
 end
